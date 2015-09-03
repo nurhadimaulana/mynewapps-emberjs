da69568594f2852f64afd6f7cc6e052e43cd9202
Nurhadi Maulana <hadi@41studio.com>
2015-09-03 17:01:22 +0700

    First commit

---
+++ app/controllers/application.js
@@ -0,0 +1,7 @@
+export default Ember.Controller.extend({
+  firstName: "Nurhadi",
+  lastName: "Maulana",
+  isLogin: true,
+  families: ["Sastiani Gita Prasastie", "Kamal Yuliandi Muhammad", "Syaila Fitria Rachmawati", "Muhammad Fikri Alhakim"],
+  enemies: []
+});
